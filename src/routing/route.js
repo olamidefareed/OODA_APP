f6d04fd485242c4f215a5e612f88fd646ab11213
Durojaiye Olamide <olamidedurojaiye724@gmail.com>
2024-11-20 16:21:57 +0100

    First page

--- src/routing/route.js
+++ src/routing/route.js
@@ -1,7 +1,8 @@
 import { lazy } from "react";
 
 const HomePage = lazy(() => import("../pages/main/home"));
-const LoginPage = lazy(()=> import("../pages/auth/Login"))
+const LoginPage = lazy(()=> import("../pages/auth/Login"));
+const ResetPasswordPage =lazy(()=>("../pages/auth/reset-password"));
 
 export const AllRoutes = [
   // Main pages
@@ -16,5 +17,10 @@ export const AllRoutes = [
     key:"/auth/login",
     component: LoginPage,
     name : "Login Page"
+  },
+  {
+    key:"/auth/reset-password",
+    component:ResetPasswordPage,
+    name : "Reset Password" 
   }
 ];
